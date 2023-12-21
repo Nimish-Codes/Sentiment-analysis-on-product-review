a6da49b439630c77ac1b1b6be5dc4ca63bc756b1
Nimish Karn <150058194+Nimish-Codes@users.noreply.github.com>
2023-12-21 12:30:38 +0545

    Update Customer_sentiment.py

--- Customer_sentiment.py
+++ Customer_sentiment.py
@@ -1,17 +1,8 @@
 import torch
-import streamlit as st
 from transformers import BertTokenizer, BertForSequenceClassification
 from transformers import AdamW, get_linear_schedule_with_warmup
 from torch.utils.data import DataLoader, TensorDataset, random_split
-
 import pandas as pd
-
-# Example labeled training data
-train_data = pd.DataFrame({
-    'review': ["This is a positive review.", "bloody shit", "Neutral comment."],
-    'label': [2, 0, 1]  # 0: Negative, 1: Neutral, 2: Positive
-})
-
 
 def fine_tune_bert_for_sentiment(train_data, num_epochs=3):
     # Load pre-trained BERT model and tokenizer
@@ -21,13 +12,13 @@ def fine_tune_bert_for_sentiment(train_data, num_epochs=3):
 
     # Tokenize and encode the training data
     inputs = tokenizer(
-        train_data['review'],
+        train_data['review'].tolist(),
         max_length=128,
         truncation=True,
         padding='max_length',
         return_tensors='pt'
     )
-    labels = torch.tensor(train_data['label'])
+    labels = torch.tensor(train_data['label'].tolist())
 
     # Create a DataLoader for training data
     dataset = TensorDataset(inputs['input_ids'], inputs['attention_mask'], labels)
@@ -56,44 +47,11 @@ def fine_tune_bert_for_sentiment(train_data, num_epochs=3):
 
     return model
 
-def analyze_sentiment_with_fine_tuned_bert(model, review):
-    # Tokenize and encode the review
-    encoded_review = tokenizer.encode_plus(
-        review,
-        max_length=128,
-        truncation=True,
-        padding='max_length',
-        return_tensors='pt'
-    )
+# Example labeled training data
+train_data = pd.DataFrame({
+    'review': ["This is a positive review.", "Negative sentiment here.", "Neutral comment."],
+    'label': [2, 0, 1]  # 0: Negative, 1: Neutral, 2: Positive
+})
 
-    # Get the model predictions
-    with torch.no_grad():
-        outputs = model(**encoded_review)
-
-    # Convert logits to probabilities
-    probabilities = torch.softmax(outputs.logits, dim=1)
-
-    # Determine the predicted sentiment
-    predicted_class = torch.argmax(probabilities, dim=1).item()
-
-    # Class 0: Negative, Class 1: Neutral, Class 2: Positive
-    if predicted_class == 0:
-        return 'Negative'
-    elif predicted_class == 1:
-        return 'Neutral'
-    else:
-        return 'Positive'
-
-# Streamlit App
-st.title('Sentiment Analysis with Fine-Tuned BERT')
-st.write('This app performs sentiment analysis on user-provided text using a fine-tuned BERT model.')
-
-# Input field for user
-user_review = st.text_area('Enter your review:')
-if st.button('Analyze Sentiment'):
-    if user_review:
-        fine_tuned_model = fine_tune_bert_for_sentiment(train_data)  # Replace train_data with your labeled training dataset
-        sentiment = analyze_sentiment_with_fine_tuned_bert(fine_tuned_model, user_review)
-        st.write(f'This is a "{sentiment}" review from the user.')
-    else:
-        st.warning('Please enter a review.')
+# Fine-tune the BERT model
+fine_tuned_model = fine_tune_bert_for_sentiment(train_data)
